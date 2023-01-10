460ccb09138d1a98fe470b8a90ebac39c8b6bc0a
Dora Gores <d.lopesgores@gmail.com>
2023-01-10 22:13:59 +0000

    fixing synthax error in fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(number)
   if number % 15 == 0
     "fizzbuzz"
-  elsif
-    number % 5 == 0
+  elsif number % 5 == 0
     "buzz"
-  elsif
-    number % 3 == 0
+  elsif number % 3 == 0
     "fizz"
   else
     number
   end
 end
+
+puts number(5)
