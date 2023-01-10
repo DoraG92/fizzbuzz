646e0b3a87005e9e11b4dac42fdae2c2b593993c
mirandaweston <mweston105@yahoo.co.uk>
2023-01-10 20:02:56 +0000

    Second RSpec test: correction

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,7 @@ describe 'fizzbuzz' do
   end
 
  describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 5' do
-      expect(fizzbuzz(5)).to eq 'fizz'
+    it 'returns "buzz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
     end
   end
