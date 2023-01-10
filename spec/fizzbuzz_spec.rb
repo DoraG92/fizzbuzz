ee3d295adba4ed760e908e95fb73441dc116f24f
Dora Gores <d.lopesgores@gmail.com>
2023-01-10 21:04:27 +0000

    fixing synthax error

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,17 +1,19 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-    it 'returns "fizz" when the number is divisible by 3' do
-      expect(fizzbuzz(3)).to eq 'fizz'
-    
+  it 'returns "fizz" when the number is divisible by 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
 
  describe 'fizzbuzz' do
-    it 'returns "buzz" when the number is divisible by 5' do
-      expect(fizzbuzz(5)).to eq 'buzz'
-  
-  describe 'fizzbuzz' do
-    it 'returns "fizzbuzz" when the number is divisible by 15' do
-      expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it 'returns "buzz" when the number is divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end 
+end
 
-    end
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when the number is divisible by 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+end
